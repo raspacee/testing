c5d794249a2bffa285b9edbdc53702feb08d43a2
BackRankCheckMate <143332200+BackRankCheckMate@users.noreply.github.com>
2024-02-15 19:43:16 +0545

    ASdad

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const indexRouter = require("./routes/index.router.js");
 const app = express();
 
 // New comment  in backen
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
